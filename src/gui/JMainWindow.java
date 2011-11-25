e2456ea4c3400b80154064b5602d6e51db05f84e
Orest Voloshchuk <orest.v@gmail.com>
2011-11-25 11:36:50 +0200

    Fixed miglayout reference

--- src/gui/JMainWindow.java
+++ src/gui/JMainWindow.java
@@ -27,6 +27,7 @@ import javax.swing.JProgressBar;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingUtilities;
+import net.miginfocom.swing.MigLayout;
 
 /**
  *
